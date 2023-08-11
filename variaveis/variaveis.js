43b11063aaa0e8ea3d4c9d5dafe31da9af71ced2
Cícero Josinaldo <31131050+c1c3r02017@users.noreply.github.com>
2023-08-11 16:48:30 -0300

    Create variaveis.js
    Resolvendo exercicio variaveis

---
+++ variaveis/variaveis.js
@@ -0,0 +1,11 @@
+const PI = 3.14159265359;
+console.log("PI:",PI);
+
+let raio = 3;
+console.log("Raio:",raio);
+
+let diametro = 2*raio;
+console.log("Diametro:",diametro);
+
+let circunferencia = 2 * PI * raio;
+console.log("Circunferencia:",circunferencia);
